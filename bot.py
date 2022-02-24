493c64b1825799088c9a4f783ebedf714dce0731
guinmoon <guinmoon@gmail.com>
2022-02-24 16:08:12 +0300

    initial

--- bot.py
+++ bot.py
@@ -12,14 +12,6 @@
 # Вероника *********, [2/22/22 17:29]
 # spider0sint_bot
 
-# BotFather, [2/22/22 17:29]
-# Done! Congratulations on your new bot. You will find it at t.me/spider0sint_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
-
-# Use this token to access the HTTP API:
-# 5268778142:AAEGVBkzPJ85KSThJ7fCTOEVCvRZoe5ueqk
-# Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
-# For a description of the Bot API, see this page: https://core.telegram.org/bots/api
 """
 
 """
